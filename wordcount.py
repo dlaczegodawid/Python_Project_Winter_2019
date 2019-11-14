17b4a008d49c454cb0ee0e6bf0a5a35f32047205
Dawid <dawidkazibut@gmail.com>
2019-11-14 14:15:46 +0100

    txt

---
+++ wordcount.py
@@ -0,0 +1,19 @@
+import string
+from collections import Counter
+f = open("mytext.txt", "r")
+tekst = f.read()
+k = tekst.split()
+
+
+newlines = []
+for item in k:
+   newlines.append(item.strip(string.punctuation))
+k = newlines
+#print(k)
+c = Counter(k)
+#print(c)
+#print(c.most_common())
+
+for m, n in c.most_common():
+    print ("{} {}" .format(m, n))
+
